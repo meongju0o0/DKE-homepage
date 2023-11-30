88d9b5a45fd8109b087bdf1f1fe6e4451f12d61e
meongju0o0 <bl4angel20@gmail.com>
2023-12-01 03:04:42 +0900

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer> {
+            <div class="footer">
+                <p>ⓒ 2023 Juyeong Shin. All rights reserved</p>
+            </div>
+        }
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav> {
+            <div class="navbar">
+                <div class = "wrapper">
+                    <div class="dropdown">
+                        <button class="dropbtn">Admissions 
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div class="dropdown-content">
+                            <a href="./pages/admissions/#">Link 1</a>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <button class="dropbtn">Members
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div class="dropdown-content">
+                            <a href="./pages/members/#">Link 1</a>
+                        </div>
+                    </div> 
+                    <div class="dropdown">
+                        <button class="dropbtn">Publications
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div class="dropdown-content">
+                            <a href="./pages/publications/#">Link 1</a>
+                        </div>
+                    </div> 
+                    <div class="dropdown">
+                        <button class="dropbtn">Research
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div class="dropdown-content">
+                            <a href="./pages/research/#">Link 1</a>
+                        </div>
+                    </div> 
+                    <div class="dropdown">
+                        <button class="dropbtn">About DKE Lab.
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div class="dropdown-content">
+                            <a href="./pages/about/#">Link 1</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            }
+        </nav>
+    );
+};
+
+export default Navbar;
