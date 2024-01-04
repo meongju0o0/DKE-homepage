4c96ab8980abfe486ea4c66d5fb02d44edd52e27
meongju0o0 <bl4angel20@gmail.com>
2024-01-04 14:06:16 +0900

    Missions Page

--- src/components/pages/about/Missions.js
+++ src/components/pages/about/Missions.js
@@ -1,20 +1,40 @@
 import React from 'react';
 
 import '../styles/Default.css';
+import '../styles/Missions.css';
+import GNN from '../../../assets/images/projects/gnn.png';
+import DistLearning from '../../../assets/images/projects/distributed-training.png';
 
 
 const Missions = () => {
     return (
         <div className="wrapper">
             <div className="main-content">
-                <h2>Missions and Visions</h2>
-                <p>Our vision is to be at the forefront of research in data and knowledge engineering, breaking down complex data patterns and transforming them into accessible and actionable knowledge.</p>
-                <p>We aim to be pioneers, driving the future of technology and shaping societal understanding of how data can be leveraged for good.</p>
-                
-                <h2>Our mission encompasses several key research areas</h2>
-                <li>Graph Neural Network Learning Engine</li>
-                <p>In the realm of Graph Neural Networks, we aim to improve the understanding and utilization of these networks. Our goal is to enhance their capacity to predict and interpret the dependencies among nodes in graph-structured data, thereby enabling more accurate predictions and deeper insights across various applications, including social network analysis, bioinformatics, and recommendation systems.</p>
-                <li></li>
+                <div className="section">
+                    <h2>Our Vision</h2>
+                    <p>Our vision is to be at the forefront of research in data and knowledge engineering, breaking down complex data patterns and transforming them into accessible and actionable knowledge.</p>
+                    <p>We aim to be pioneers, driving the future of technology and shaping societal understanding of how data can be leveraged for good.</p>
+                </div>
+
+                <div className="section">
+                    <h2>Our Missions</h2>
+                    <ul>
+                        <li>Graph Neural Network</li>
+                        <img src={GNN} alt="GNN" />
+                        <p>In the realm of Graph Neural Networks, we aim to improve the understanding and utilization of these networks. Our goal is to enhance their capacity to predict and interpret the dependencies among nodes in graph-structured data, thereby enabling more accurate predictions and deeper insights across various applications, including social network analysis, bioinformatics, and recommendation systems.</p>
+                        <li>Distributed Learning Engine</li>
+                        <img src={DistLearning} alt="DistLearning" />
+                        <p>As part of our work on Distributed Training, we seek to revolutionize the scalability and performance of machine learning algorithms. By training these algorithms across multiple servers or nodes, we aim to enhance the speed and sophistication of data processing and model training.</p>
+                        <li>In-Memory DBMS Query Optimization</li>
+                        {/*
+                        <img src={ } alt="DBMS" />*/}
+                        <p>When it comes to In-Memory Database Procedure Optimization, we are committed to refining these procedures for improved efficiency and speed. Our work is focused on creating innovative methods that can enhance transaction speeds and promote better usage of resources.</p>
+                        <li>Natural Language Based Searching Engine Development</li>
+                        {/*
+                        <img src={ } alt="NLP" />*/}
+                        <p>Finally, our research into Emotion Detection is dedicated to understanding and interpreting human emotions more accurately. Utilizing advanced machine learning and natural language processing techniques, we are working to create systems that can empathetically and accurately respond to human emotions.</p>
+                    </ul>
+                </div>
             </div>
         </div>
     );
