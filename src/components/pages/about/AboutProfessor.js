b4b3c8f95a1dc4e630b40e70eaea1749d5695120
meongju0o0 <bl4angel20@gmail.com>
2024-01-04 15:03:36 +0900

    Update about professor

--- src/components/pages/about/AboutProfessor.js
+++ src/components/pages/about/AboutProfessor.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
+import '../styles/Default.css';
+import '../styles/AboutProfessor.css';
+
 
 const AboutProfessor = () => {
     return (
-        <div>
-
+        <div className="wrapper">
+            <div className="main-content">
+                <div className = "intro">
+                    <div className="img-intro">
+                        <img src="/images/profile-images/professor.png" alt="Prof. Young-Koo Lee"/>
+                    </div>
+                    <div className="text-intro">
+                        <h2>Prof. Young-Koo Lee</h2>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
